e8880b2f13b70fa0dc69bb899ca961cfe0c7952b
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-01-01 16:50:32 +0530

    Add theme toggle functionality

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,28 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Moon, Sun } from "lucide-react";
+import { motion } from "framer-motion";
 
 const ThemeToggle = () => {
-  const current = 0;
+  const [current, setCurrent] = useState(0);
   return (
     <div className="h-full w-fit flex border-l border-black flex-col group">
-      <Button enabled={current == 0}>
+      <motion.div
+        initial={{
+          backgroundColor: "#f40c3f",
+          height: "100%",
+        }}
+        animate={{
+          height: current == 0 ? "100%" : "0%",
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.83, 0, 0.17, 1],
+        }}
+        class=" mix-blend-multiply fixed top-0 left-0 z-50 size-full pointer-events-none"
+      ></motion.div>
+      {/* <Button onClick={() => setCurrent(0)} enabled={current == 0}>
         <Moon height={"50%"}></Moon>
       </Button>
       <div className="h-[1px] w-full bg-black shrink-0"></div>
-      <Button enabled={current == 1}>
+      <Button onClick={() => setCurrent(1)} enabled={current == 1}>
         <Sun height={"50%"}></Sun>
-      </Button>
+      </Button> */}
+      <div onClick={() => setCurrent(current == 0 ? 1 : 0)} className="h-full cursor-pointer flex flex-col relative">
+        <motion.div
+          animate={{
+            clipPath:
+              current == 0
+                ? "polygon(0% 0%, 100% 0%, 100% 50%, 0% 50%)"
+                : "polygon(0% 50%, 100% 50%, 100% 100%, 0% 100%)",
+          }}
+          transition={{
+            duration: 0.2,
+            ease: [0.83, 0, 0.17, 1],
+          }}
+          className="h-full flex flex-col absolute z-20 pointer-events-none"
+        >
+          <button className="aspect-square bg-black text-white shrink-0 h-1/2 flex items-center justify-center">
+            <Moon></Moon>
+          </button>
+          <div className="h-[1px] w-full bg-black shrink-0"></div>
+          <button className="aspect-square bg-black text-white shrink-0 h-1/2 flex items-center justify-center">
+            <Sun></Sun>
+          </button>
+        </motion.div>
+        <button
+          // onClick={() => setCurrent(0)}
+          className="aspect-square shrink-0 h-1/2 flex items-center justify-center"
+        >
+          <Moon></Moon>
+        </button>
+        <div className="h-[1px] w-full bg-black shrink-0"></div>
+        <button
+          // onClick={() => setCurrent(1)}
+          className="aspect-square shrink-0 h-1/2 flex items-center justify-center"
+        >
+          <Sun></Sun>
+        </button>
+      </div>
     </div>
   );
 };
 
-const Button = ({ children, enabled }) => {
+const Button = ({ children, enabled, onClick }) => {
   return (
     <>
       <button
+        onClick={onClick}
         className={`h-1/2 aspect-square shrink-0 flex items-center justify-center ${
           enabled ? "bg-black text-white" : "hover:bg-black hover:text-surface"
-        } border-border`}
+        } border-border transition-colors`}
       >
         {children}
       </button>
